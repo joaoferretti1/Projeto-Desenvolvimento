50ed0eaadbb2a6fe471e4be3ad3a026a64aa0933
joaoferretti1 <jfcarvalho@minha.fag.edu.br>
2026-09-01 14:46:32 -0300

    feito atv 5

---
+++ Atv5/calculadora.js
@@ -0,0 +1,8 @@
+const valorConta = 100;
+const percentualGorgeta = 15;
+let gorgeta = valorConta * (percentualGorgeta/100)
+let total = valorConta + gorgeta
+
+console.log("Valor da Conta: R$", valorConta, 
+    "Gorgeta(15%): R$", gorgeta,
+     "Total a pagar: R$", total)
